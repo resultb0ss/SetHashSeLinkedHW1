672fbae55a728dbb63854627af68ff5fee68f3f5
Admin <reteamboss@gmail.com>
2024-12-04 14:43:12 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.LinkedHashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LinkedHashSet<String> colorsOne = new LinkedHashSet<>();
+        LinkedHashSet<String> colorsTwo = new LinkedHashSet<>();
+
+
+        colorsOne.add("Red");
+        colorsOne.add("Blue");
+        colorsOne.add("Green");
+        colorsOne.add("White");
+
+        colorsTwo.add("White");
+        colorsTwo.add("Violet");
+        colorsTwo.add("Green");
+        colorsTwo.add("Grey");
+
+
+        System.out.println(unitedColors(colorsOne, colorsTwo));
+
+
+    }
+
+    public static LinkedHashSet<String> unitedColors(LinkedHashSet<String> colorsOne, LinkedHashSet<String> colorsTwo){
+        LinkedHashSet<String> unitedColorsSet;
+        colorsOne.retainAll(colorsTwo);
+        unitedColorsSet = colorsOne;
+        return unitedColorsSet;
     }
 }
